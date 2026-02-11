90ade62398a9603976c57ec06e7339ad1129214a
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-11 12:33:18 +0545

    Add skills section: Create components for displaying skills and technologies, including SkillCard, SkillCategoryCard, and SkillsSection. Implement animations for skill presentation and organize skills into categories for better user experience.

---
+++ src/components/skills/skill-card.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/utils";
+import { IStack } from "@/utils/stack";
+import { motion } from "motion/react";
+import Image from "next/image";
+
+function SkillCard({ skill, index }: { skill: IStack; index: number }) {
+  const typeOfSkillIcon = skill.icon instanceof Function ? "icon" : "image";
+
+  return (
+    <motion.div
+      key={skill.label}
+      initial={{ opacity: 0, scale: 0.8 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      transition={{ delay: index * 0.05 }}
+      viewport={{ once: true }}
+      className={cn(
+        "group/item flex items-center gap-2 border border-dashed border-neutral-400 p-2 transition-colors duration-200 dark:border-neutral-600",
+        index % 2 === 0 ? "bg-linear-l" : "bg-linear-b"
+      )}>
+      <div className="p-1.5">
+        {typeOfSkillIcon === "icon" ? (
+          <skill.icon className="text-accent-foreground size-5" />
+        ) : (
+          <Image
+            src={skill.icon as string}
+            alt={skill.label}
+            width={20}
+            height={20}
+            className="text-accent-foreground size-5 invert"
+          />
+        )}
+      </div>
+      <span className="text-accent-foreground truncate text-base font-medium">
+        {skill.label}
+      </span>
+    </motion.div>
+  );
+}
+
+export { SkillCard };
