c16ffd357ed46db550f979a78aac00b5bac2cca7
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-11 12:32:53 +0545

    Add projects section: Create pages for project overview and individual project details, including metadata generation. Introduce reusable components for project cards and image galleries, enhancing the showcase of web development work.

---
+++ src/components/projects/project-card.tsx
@@ -0,0 +1,329 @@
+"use client";
+import { Card, CardContent } from "@/components/ui/8bit/card";
+import { Check, LinkIcon } from "lucide-react";
+import type { Tech } from "@/components/projects/tech-badge";
+import type { Project } from "@/data/projects";
+import { TechBadge } from "@/components/projects/tech-badge";
+import { DetailSwapCard } from "@/components/projects/card";
+import type { Route } from "next";
+
+import { motion } from "motion/react";
+
+import Image from "next/image";
+
+import Link from "next/link";
+import { ArrowUpRight } from "lucide-react";
+import { SiGithub } from "react-icons/si";
+
+import { TECH_ICONS, TechStack } from "@/utils/icon-map";
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger
+} from "../ui/8bit/tooltip";
+import { Button } from "../ui/8bit/button";
+
+export function ProjectCard({
+  project,
+  details = false
+}: {
+  project: Project;
+  details?: boolean;
+}) {
+  if (!details) {
+    return (
+      <motion.div className="group relative border border-neutral-500/40 p-4 transition-all">
+        <div className="flex flex-col gap-6 md:flex-row">
+          <Link
+            href={`/projects/${project.slug}`}
+            className="relative block aspect-video h-48 w-full shrink-0 overflow-hidden md:w-76">
+            <Image
+              src={project.thumbnail}
+              alt={project.title}
+              fill
+              className="object-cover transition-transform duration-500"
+            />
+          </Link>
+
+          <div className="flex flex-1 flex-col justify-between py-1">
+            <div className="space-y-3">
+              <h3 className="group-hover:text-primary text-xl font-bold transition-colors md:text-2xl">
+                {project.title}
+              </h3>
+              <p className="text-muted-foreground mb-4 line-clamp-2 md:line-clamp-3">
+                {project.description}
+              </p>
+
+              <div className="flex mt-4 flex-wrap gap-4">
+                {project.technologies.map((tech) => {
+                  const Icon =
+                    TECH_ICONS[tech.name.toUpperCase() as TechStack] ||
+                    TECH_ICONS["DEFAULT"];
+                  return (
+                    <TooltipProvider key={tech.name}>
+                      <Tooltip>
+                        <TooltipTrigger
+                          asChild
+                        >
+                          <Icon className="size-6" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>{tech.name}</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+
+                  );
+                })}
+
+              </div>
+
+              <div className="mt-4 items-center flex-wrap gap-6 flex justify-end">
+                <ProjectLinks project={project} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    );
+  }
+
+  return (
+    <motion.div
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5 }}
+      className="h-full">
+      <Card className="h-full gap-3 overflow-hidden border-transparent dark:bg-transparent">
+        <div className="relative mb-1 aspect-video">
+          <DetailSwapCard
+            data={{
+              images: project.images
+            }}
+            imageHeight={480}
+            imageFit="cover"
+            showThumbnailNavigator
+            showDotIndicator
+            showImageCounter={false}
+          />
+        </div>
+
+        <CardContent className="space-y-6 border-0 p-4">
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            className="flex items-start justify-between">
+            <h3 className="text-xl font-medium md:text-2xl">{project.title}</h3>
+          </motion.div>
+
+          <motion.p
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            className="text-muted-primary text-[15px] leading-relaxed">
+            {project.description}
+          </motion.p>
+
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            className="space-y-3">
+            <motion.h4
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.3, ease: "easeOut" }}
+              className="text-muted-primary text-base font-semibold">
+              Key Features:
+            </motion.h4>
+            <motion.ul
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.3, ease: "easeOut" }}
+              className="flex flex-col space-y-2">
+              {project.features.map((feature: string, idx: number) => (
+                <motion.li
+                  key={idx}
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  viewport={{ once: true }}
+                  transition={{ duration: 0.3, ease: "easeOut" }}
+                  className="text-muted-foreground flex items-center text-base font-medium">
+                  <Check className="mr-2 h-4 w-4" /> {feature}
+                </motion.li>
+              ))}
+            </motion.ul>
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            className="space-y-3">
+            <motion.h4
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.3, ease: "easeOut" }}
+              className="text-muted-primary text-base font-semibold">
+              Technologies:
+            </motion.h4>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.3, ease: "easeOut" }}
+              className="flex flex-wrap gap-2">
+              {project.technologies.map((tech: Tech, idx: number) => {
+                const Icon =
+                  TECH_ICONS[tech.name.toUpperCase() as TechStack] ||
+                  TECH_ICONS["DEFAULT"];
+                return (
+                  <TechBadge key={idx} className="text-base">
+                    <div className="flex items-center gap-2">
+                      <Icon className="size-4" />
+                      {tech.name}
+                    </div>
+                  </TechBadge>
+                );
+              })}
+            </motion.div>
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            className="space-y-3">
+            <motion.h4
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.3, ease: "easeOut" }}
+              className="text-muted-primary text-base font-semibold">
+              Links:
+            </motion.h4>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.3, ease: "easeOut" }}
+              className="group flex flex-wrap gap-6">
+              <ProjectLinks project={project} details />
+            </motion.div>
+          </motion.div>
+        </CardContent>
+      </Card>
+    </motion.div>
+  );
+}
+
+export function ProjectLinks({
+  details = false,
+  project
+}: {
+  details?: boolean;
+  project: Project;
+}) {
+  if (!details) {
+    return (
+      <div className="flex items-center gap-3">
+        {project.liveUrl && (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger
+                asChild
+              ><Link
+                href={project.liveUrl as Route}
+                target="_blank"
+                title="Live Demo">
+                  <LinkIcon className="size-6" />
+                </Link></TooltipTrigger>
+              <TooltipContent>
+                <p>Live Demo</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        )}
+        {project.githubUrl && (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger
+                asChild
+              ><Link
+                href={project.githubUrl as Route}
+                target="_blank"
+                title="Source Code">
+                  <SiGithub className="size-6" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Source Code</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        )}
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger
+              asChild
+            >
+              <Link
+                href={`/projects/${project.slug}`}
+                title="Project Details">
+                <ArrowUpRight className="size-6" />
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Project Details</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+    );
+  }
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.3, ease: "easeOut" }}
+      className="group flex flex-wrap gap-6">
+      {project.liveUrl && (
+        <Button
+          asChild
+          className="group hover:shadow-primary relative py-2 font-normal">
+          <Link
+            href={project.liveUrl as Route}
+            target="_blank"
+          >
+            Live Demo
+          </Link>
+        </Button>
+      )}
+
+      {project.githubUrl && (
+        <Button
+          asChild
+          className="group hover:shadow-primary relative py-2 font-normal">
+          <Link
+            href={project.githubUrl as Route}
+          >
+            Source Code
+          </Link>
+        </Button>
+
+      )}
+    </motion.div>
+  );
+}
---
+++ src/components/projects/tech-badge.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import { Badge } from "../ui/8bit/badge";
+
+export interface Tech {
+    name: string;
+}
+
+export function TechBadge({
+    children,
+    className
+}: {
+    children: React.ReactNode;
+    className?: string;
+}) {
+    return (
+        <Badge
+            className={cn(
+                className
+            )}
+        >
+            {children}
+        </Badge>
+    );
+}
